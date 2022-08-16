1338d81dcda9f18cb5aaaee6a1d72e192064716a
Darksuger6 <110680319+Darksuger6@users.noreply.github.com>
2022-08-17 05:25:41 +0800

    Add files via upload

--- Project3/Project3/源.cpp
+++ Project3/Project3/源.cpp
@@ -3,16 +3,30 @@ using namespace std;
 int main()
 {
 	int a = 10;
-	int b = 10;
-	int a1 = a-- * 10;
-	int a2 = a1--;
-	int b1 = --b * 10;
-	int b2 = --b1;
+	a += 2;
 	cout << "a=" << a << endl;
-	cout << "b=" << b << endl;
-	cout << "a1=" << a1 << endl;
-	cout << "a2=" << a2 << endl;
-	cout << "b1=" << b1 << endl;
-	cout << "b2=" << b2 << endl;
+	
+	a = 6;
+	a -= 2;
+	cout << "a=" << a << endl;
+
+	a = 2;
+	a *= 2;
+	cout << "a=" << a << endl;
+
+	a = 6;
+	a %= 3;
+	cout << "a=" << a << endl;
+
+	
+	int b = 20;
+	a = 0;
+	cout << (a==b) << endl;
+	cout << !a << endl;
+	cout << (a&&b) << endl;
+	cout << (a || b) << endl;
+
+
+	system("pause");
 	
 }
